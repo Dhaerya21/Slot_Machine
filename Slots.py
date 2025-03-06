af597b273313af7bcf7f83cf92cf71e3875668ab
Dhaerya21 <dhaeryakhanna01@gmail.com>
2025-03-06 23:30:45 +0530

    Completed slot main functions

--- Slots.py
+++ Slots.py
@@ -1,10 +1,21 @@
 import random as r
+from Slots_art import symbols,payout
 
 def show_slots(slot,balance):
-    pass
+    print("***************************")
+    print(" | ".join(slot))
+    balance =Wins(slot,balance)
+    print("***************************")
+    return balance
 
 def slots(balance):
-    pass
+    if balance < 10:
+        print("Insufficient funds! Please Deposit ")
+        return balance
+    balance-=10
+    slot = [r.choice(symbols) for i in range(3)]
+    balance=show_slots(slot,balance)
+    return balance
 
 def Wins(slot,balance):
     pass
