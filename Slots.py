c45044396b9fde78b4d2ede036545d65c15e2c5c
Dhaerya21 <dhaeryakhanna01@gmail.com>
2025-03-06 23:28:43 +0530

    Created file for art and managing payout

---
+++ Slots.py
@@ -0,0 +1,17 @@
+import random as r
+
+def show_slots(slot,balance):
+    pass
+
+def slots(balance):
+    pass
+
+def Wins(slot,balance):
+    pass
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()       
---
+++ Slots_art.py
@@ -0,0 +1,11 @@
+#this space can be used for art and changing payout
+symbols= ["🍒","💩","💰","🪙","💕","🔔","👻","🐍"]
+
+def payout():
+    print("***************************")
+    print("slots payout")
+    print("3 symbols : Jackpot!!!")
+    print("2 sympbol : credits back")
+    print("1 symbol : no win\n")
+    print("10 credits per play")
+    print("***************************")
